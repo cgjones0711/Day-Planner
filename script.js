150d80124ba81fb234b998c83ead768e06fcadd7
Clay Jones <cgjones0711@gmail.com>
2021-03-25 23:22:28 -0500

    color-change close

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-var currentDate = moment().format("HH");
-console.log(currentDate)
+let currentHour = moment().format("HH");
+console.log(currentHour)
+
+let timeBlocks = [
+{"0900"};
+{"1000"};
+{"1100"};
+{"1200"};
+{"1300"};
+{"1400"};
+{"1500"} ; 
+{"1600"};
+{"1700"};
+]
+
+for (let i = 0; i < timeBlocks.length; i++) {
+    let timeBlocks=document.getElementById
+    function changeColors(){
+        if (timeBlocks == currentHour){
+            timeBlocks.addclass(".present")
+        }
+        if (timeBlocks > currentHour){
+            timeBlocks.addclass(".future")
+    }
+    if (timeBlocks , currentHour){
+        timeBlocks.addclass(".past")
+
+    
+    }
+} 
+}
