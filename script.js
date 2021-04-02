42a77e4d699955fd86d39dc85c5c5b2ce1c0f223
Clay Jones <cgjones0711@gmail.com>
2021-04-01 21:49:02 -0500

    added numbers

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
 let currentHour = moment().format("HH");
 console.log(currentHour)
-let timeBlocks=document.getElementsByClassName(".description")
-
-for (let i = 0; i < timeBlocks.length; i++) {
+let hours = ["0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300","1400","1500","1600","1700","1800","1900","2000","2100","2200","2300","2400"];
+let timeRows=document.getElementsByClassName(".future")
+for (let i = 0; i < hours.length; i++) {
     
     function changeColors(){
-        if (timeBlocks == currentHour){
-            timeBlocks.addclass(".present")
+        if (hours == currentHour){
+            timeRows.addclass(".present")
         }
-        if (timeBlocks > currentHour){
-            timeBlocks.addclass(".future")
+        if (hours > currentHour){
+            timeRows.addclass(".future")
     }
-    if (timeBlocks < currentHour){
-        timeBlocks.addclass(".past")
+    if (hours < currentHour){
+        timeRows.addclass(".past")
+        console.log(timeRows.addclass(".past"))
 
     
     }
