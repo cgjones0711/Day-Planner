5500d55090337a5df7ce245ca16e341408610a52
Clay Jones <cgjones0711@gmail.com>
2021-04-01 21:14:29 -0500

    change colors function

--- script.js
+++ script.js
@@ -2,21 +2,10 @@ $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
 let currentHour = moment().format("HH");
 console.log(currentHour)
-
-let timeBlocks = [
-"0900",
-"1000",
-"1100",
-"1200",
-"1300",
-"1400",
-"1500", 
-"1600",
-"1700",
-]
+let timeBlocks=document.getElementsByClassName(".description")
 
 for (let i = 0; i < timeBlocks.length; i++) {
-    let timeBlocks=document.getElementById(".time-block row ")
+    
     function changeColors(){
         if (timeBlocks == currentHour){
             timeBlocks.addclass(".present")
@@ -32,3 +21,4 @@ for (let i = 0; i < timeBlocks.length; i++) {
 } 
 }
 console.log
+    
