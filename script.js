2b06e8305b8854bf521fb292de5c3d6058676bf4
Clay Jones <cgjones0711@gmail.com>
2021-04-08 18:42:47 -0500

    set listener to body

--- script.js
+++ script.js
@@ -28,16 +28,8 @@ let hourLoop = document.getElementById("#hourloop");
 let saveBtn = document.querySelector("#save");
 let comments=document.querySelector("comments")
 
-
-
-
-
-
-
-
-
-
-
+document.body.addEventListener("click", function(event) {
+event.preventDefault();
 
 
 
@@ -50,8 +42,16 @@ let appointmentMessage = {
   messageInput: messageInput.value.trim()
 };
 
-localStorage.setItem("appointmentMessage", JSON.stringify(appointmentMessage));
+localStorage.setItem("9", JSON.stringify(appointmentMessage));
+localStorage.setItem("10", JSON.stringify(appointmentMessage));
+localStorage.setItem("11", JSON.stringify(appointmentMessage));
+localStorage.setItem("12", JSON.stringify(appointmentMessage));
+localStorage.setItem("13", JSON.stringify(appointmentMessage));
+localStorage.setItem("14", JSON.stringify(appointmentMessage));
+localStorage.setItem("15", JSON.stringify(appointmentMessage));
+localStorage.setItem("16", JSON.stringify(appointmentMessage));
+localStorage.setItem("17", JSON.stringify(appointmentMessage));
 
 });
 
-
+})
